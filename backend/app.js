a540c25adc46fee8e7654c0a3d81bdc7334540fb
Shahzad Ali <shahzadmobi619@gmail.com>
2025-07-08 00:26:52 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const path = require("path");
 app.use(express.json());
 app.use(cookieParser()); 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://multi-vendor-frontend-mocha.vercel.app/"],
     credentials: true,
   }));
   
