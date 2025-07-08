683c7cc1de6f875476b3f3e438d057cdbcbc631d
Shahzad Ali <shahzadmobi619@gmail.com>
2025-07-08 00:40:34 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,39 @@
 
-const { log } = require("console");
-const  app = require("./app");
-const connectDatabase = require("./db/Database");
+// const { log } = require("console");
+// const  app = require("./app");
+// const connectDatabase = require("./db/Database");
 
 
 
-//Handling uncaught exception
-process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`shoting down the server for handling uncaught exeption`); 
+// //Handling uncaught exception
+// process.on("uncaughtException", (err) => {
+//     console.log(`Error: ${err.message}`);
+//     console.log(`shoting down the server for handling uncaught exeption`); 
     
-})
+// })
 
-//config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-    require ("dotenv").config({
-        path:"config/.env"
-    });
-}
+// //config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//     require ("dotenv").config({
+//         path:"config/.env"
+//     });
+// }
 
-// connect db
-connectDatabase();
+// // connect db
+// connectDatabase();
 
-//create server
-const server = app.listen(process.env.PORT, () => {
-console.log(`server is running on http://localhost:${process.env.PORT} `);
+// //create server
+// const server = app.listen(process.env.PORT, () => {
+// console.log(`server is running on http://localhost:${process.env.PORT} `);
 
-})
+// })
 
-// unhandled promise rejection 
-process.on("unhandledRejection", (err) => {
-    console.log(`shoting down the server for ${err.message}`);
-    console.log(`shoting down the server for unhandled promise rejection`);
+// // unhandled promise rejection 
+// process.on("unhandledRejection", (err) => {
+//     console.log(`shoting down the server for ${err.message}`);
+//     console.log(`shoting down the server for unhandled promise rejection`);
 
-    server.close(() => {
-        process.exit(1);
-    });
-});
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
